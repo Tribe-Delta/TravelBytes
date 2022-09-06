0c811646fdc809f21a9ee6f6c97192c7b4699659
Dawesome101 <dawesome@danawesome.com>
2022-09-06 11:20:47 -0700

    Changed App to show login only until authorized and show logout only once authorized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
 import Login from './Login.js'
 import Logout from './Logout.js'
-import Profile from './Profile.js';
+import Profile from './Profile.js'
+import Content from './Content.js';
 import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>This is the page.</p>
-      <Login />
-      <Logout />
-      <Profile />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>This is the page.</p>
+        {this.props.auth0.isAuthenticated ? 
+          <>
+            <Profile />
+            {/* <Content /> */}
+            <Logout />
+          </>
+          :
+          <Login />
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuth0(App);
